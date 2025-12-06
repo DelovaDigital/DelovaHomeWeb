3a35cbba008f83b21fce0a90ade08c98bda3eb9d
AlessioD <106388894+AlessioD200@users.noreply.github.com>
2025-12-06 12:04:53 +0100

    Update cameraStream.js

--- script/cameraStream.js
+++ script/cameraStream.js
@@ -36,12 +36,16 @@ startFFmpeg() {
     "-rtsp_transport", "tcp",
     "-i", this.url,
     "-an",
-    "-c:v", "copy",
+    "-c:v", "libx264",
+    "-preset", "ultrafast",
+    "-tune", "zerolatency",
+    "-profile:v", "baseline",
+    "-level", "3.1",
     "-fflags", "+genpts",
     "-f", "rtp",
     "-payload_type", "96",
     `rtp://127.0.0.1:${this.udpPort}?pkt_size=1200`
-];
+];s
 
     console.log(`[WebRTC] Starting ffmpeg: ffmpeg ${args.join(" ")}`);
     this.ffmpeg = spawn("ffmpeg", args);
